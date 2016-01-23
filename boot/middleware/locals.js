2886598ef6f6df13b652f83ec536ec9fb5f43b14
thealien <roman.ziva@gmail.com>
2016-01-23 22:48:45 +0300

    mincss if PROD

--- boot/middleware/locals.js
+++ boot/middleware/locals.js
@@ -1,6 +1,7 @@
 module.exports = function (app, config) {
 
     app.locals.paths = config.app.paths;
+    app.locals.IS_PROD = app.isProd;
 
     app.use(function (req, res, next) {
         if (!app.locals.baseUrl) {
