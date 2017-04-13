cd73be0a90da1754ccf2ad00de1847e734740d59
thealien <roman.ziva@gmail.com>
2017-04-13 21:05:26 +0300

    move models to files

---
+++ models/Image.js
@@ -0,0 +1,111 @@
+module.exports = function(db, DataTypes) {
+
+    const Image = db.define('images', {
+        /*
+         PRIMARY KEY (`id`),
+         UNIQUE KEY `guid` (`path_date`,`guid`) USING BTREE,
+         KEY `userid_IDX` (`uploaduserid`),
+         KEY `date_group_IDX` (`path_date`,`group`),
+         KEY `delguid` (`path_date`,`deleteGuid`) USING BTREE
+         */
+
+        // `id` int(11) NOT NULL AUTO_INCREMENT
+        id: {
+            type:           DataTypes.INTEGER.UNSIGNED,
+            autoIncrement:  true,
+            primaryKey:     true,
+            allowNull:      false
+        },
+
+        // `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        date: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW
+        },
+
+        // `filename` text NOT NULL
+        filename: {
+            type: DataTypes.STRING(32),
+            allowNull:      false
+        },
+
+        // `ip` varchar(15) NOT NULL
+        ip: {
+            type: DataTypes.STRING(15),
+            allowNull:      false
+        },
+
+        // `deleteGuid` varchar(32) NOT NULL
+        deleteGuid: {
+            type: DataTypes.STRING(32),
+            allowNull:      false
+        },
+
+        // `status` tinyint(4) NOT NULL DEFAULT '0'
+        status: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+
+        // `width` int(11) NOT NULL DEFAULT '0'
+        width: {
+            type:           DataTypes.INTEGER.UNSIGNED,
+            allowNull:      false
+        },
+
+        // `height` int(11) NOT NULL DEFAULT '0'
+        height: {
+            type:           DataTypes.INTEGER.UNSIGNED,
+            allowNull:      false
+        },
+
+        // `uploaduserid` int(10) unsigned NOT NULL DEFAULT '0'
+        uploaduserid: {
+            type:           DataTypes.INTEGER.UNSIGNED,
+            allowNull:      false
+        },
+
+        // `filesize` int(10) unsigned NOT NULL DEFAULT '0'
+        filesize: {
+            type:           DataTypes.INTEGER.UNSIGNED,
+            allowNull:      false
+        },
+
+        // `preview` int(10) unsigned NOT NULL DEFAULT '0'
+        preview: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+
+        // `originalfilename` varchar(255) NOT NULL
+        originalfilename: {
+            type: DataTypes.STRING
+        },
+
+        // `useragent` tinyint(2) unsigned NOT NULL DEFAULT '0' COMMENT '0 - browser, 1 - ihuploader',
+        useragent: {
+            type:           DataTypes.INTEGER(1)
+        },
+
+        // `guid` varchar(32) NOT NULL COMMENT 'image GUID (part of filename)'
+        guid: {
+            type: DataTypes.STRING(32),
+            allowNull:      false
+        },
+
+        // `path_date` varchar(8) NOT NULL DEFAULT ''
+        path_date: {
+            type: DataTypes.STRING(8),
+            allowNull:      false
+        },
+
+        // `group` varchar(32) NOT NULL DEFAULT '',
+        group: {
+            type: DataTypes.STRING(32),
+            allowNull:      false
+        }
+    });
+
+    return Image;
+
+};
---
+++ models/User.js
@@ -0,0 +1,99 @@
+const utils = require('../utils');
+
+module.exports = function(db, DataTypes) {
+
+    const User = db.define('users', {
+        /*
+         PRIMARY KEY (`userID`),
+         UNIQUE KEY `username` (`username`),
+         UNIQUE KEY `email` (`email`)
+         */
+
+        // `userID` mediumint(8) unsigned NOT NULL AUTO_INCREMENT
+        userID: {
+            type:           DataTypes.INTEGER.UNSIGNED,
+            autoIncrement:  true,
+            primaryKey:     true,
+            allowNull:      false
+        },
+
+        // `username` varchar(50) DEFAULT NULL
+        username: {
+            unique:     true,
+            type: DataTypes.STRING(50),
+            allowNull:      false,
+            notEmpty: true,
+            validate: {
+                isUnique: (username, done) => {
+                    username = String(username).trim();
+                    const query = User.find({ where: {username: username}, attributes: ['userID']});
+                    query
+                        .then(record => {
+                            const error = record ? new Error('Логин уже занят') : null;
+                            done(error);
+                        })
+                        .catch(done);
+                }
+            }
+        },
+
+        // `password` varchar(100) DEFAULT NULL
+        password: {
+            type: DataTypes.STRING(50),
+            allowNull:      false,
+            set: function (password) {
+                this.setDataValue('password', utils.md5(password));
+            },
+            validate: {
+                notEmpty: true
+            }
+        },
+
+        // `email` varchar DEFAULT NULL,
+        email: {
+            type: DataTypes.STRING,
+            allowNull:      false,
+            unique: true,
+            validate: {
+                isEmail: {
+                    msg: 'Email имеет неверный формат'
+                },
+                isUnique: (email, done) => {
+                    email = String(email).trim();
+                    const query = User.find({ where: {email: email}, attributes: ['userID']});
+                    query
+                        .then(record => {
+                            const error = record ? new Error('Email уже занят') : null;
+                            done(error);
+                        })
+                        .catch(done);
+                }
+            }
+        },
+
+        // `role` enum('admin','user') NOT NULL DEFAULT 'user',
+        role: {
+            type: DataTypes.ENUM('admin', 'user'),
+            defaultValue: 'user',
+            allowNull:      false
+        },
+
+        // `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        date: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW
+        }
+
+    }, {
+        instanceMethods: {
+            samePassword: function (password) {
+                return this.password === utils.md5(password);            },
+            isAdmin: function () {
+                return this.role === 'admin';
+            }
+        }
+    });
+
+    return User;
+
+};
